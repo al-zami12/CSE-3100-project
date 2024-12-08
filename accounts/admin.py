5f9729bf12b8ea317b6051a60ec29e1fdbcb7ad0
al-zami12 <101575328+al-zami12@users.noreply.github.com>
2024-12-08 17:04:26 +0600

    initial commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
-from .models import Profile, Cart, CartItem, Order, OrderItem
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
 
-admin.site.register(Profile)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+from accounts.forms import UserChangeForm
+from accounts.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ('full_name', 'phone_number', 'email', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        ('Main', {'fields': ('full_name', 'email', 'phone_number', 'password')}),
+        ('Personal info', {'fields': ('is_active',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('full_name', 'phone_number', 'email', 'password1', 'password2')
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,39 +1,48 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.models import User
-from accounts.models import Profile
-from home.models import ShippingAddress
+from accounts.models import User
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['profile_image', 'bio']
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
 
-
-class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ('email', 'full_name', 'phone_number')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise forms.ValidationError('passwords must match')
+        return cd['password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
 
 
-class ShippingAddressForm(forms.ModelForm):
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
     class Meta:
-        model = ShippingAddress
-        fields = '__all__'
-        exclude = ['user']
+        model = User
+        fields = ('email', 'password', 'full_name', 'phone_number')
+
+    def clean_password(self):
+        return self.initial['password']
 
 
-class CustomPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(
-        label="Current password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-    )
-    new_password1 = forms.CharField(
-        label="New password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-    )
-    new_password2 = forms.CharField(
-        label="New password confirmation",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-    )
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UserRegistrationForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone_number = forms.CharField(max_length=12,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
